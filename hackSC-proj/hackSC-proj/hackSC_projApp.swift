3c8b5cd7d5b833175c1a418be685c7b9ab40394d
Jessica Fu <53541136+jjessicaf@users.noreply.github.com>
2023-02-04 13:17:36 -0800

    Add files via upload

---
+++ hackSC-proj/hackSC-proj/hackSC_projApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackSC_projApp.swift
+//  hackSC-proj
+//
+//  Created by Jessica Fu on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct hackSC_projApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hackSC-proj/hackSC-projUITests/hackSC_projUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  hackSC_projUITestsLaunchTests.swift
+//  hackSC-projUITests
+//
+//  Created by Jessica Fu on 2/4/23.
+//
+
+import XCTest
+
+final class hackSC_projUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
